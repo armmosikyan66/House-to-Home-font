fddcbade49a30c195c0d44d9fb91a49432046139
armmosikyan66 <armanmosikyan@gmail.com>
2023-04-09 21:36:49 +0400

    try to deploy

--- utils/constants/api.ts
+++ utils/constants/api.ts
@@ -1,2 +1,5 @@
-// del url
-export const API_URL = `http://localhost:3001/api`
+// dev url
+// export const API_URL = `http://localhost:5000/api`;
+
+// prd url
+export const API_URL = "https://house-to-home-back.vercel.app/api"
