338a1f47c40766ff13d2bcd16c0f47778bf9586c
armmosikyan66 <armanmosikyan@gmail.com>
2023-04-15 15:51:38 +0400

    check link

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,15 @@ import type {GetStaticProps, NextPage} from 'next';
 import {serverSideTranslations} from "next-i18next/serverSideTranslations";
 import {Home} from '../component/templates/app/home';
 import nextI18NextConfig from '../next-i18next.config.js';
+import { useEffect } from 'react';
+import $api from '../utils/http';
 const HomePage: NextPage<{}> = () => {
+    useEffect(() => {
+        (async () => {
+            const qwe = $api.post("/check");
+            console.info(qwe)
+        })()
+    }, []) 
     return (
         <>
             <Home.Intro />
