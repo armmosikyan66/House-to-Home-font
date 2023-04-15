dea51ee4769fdc5e2119d915109936b6524c9097
armmosikyan66 <armanmosikyan@gmail.com>
2023-04-15 16:33:13 +0400

    qwe

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,15 +2,8 @@ import type {GetStaticProps, NextPage} from 'next';
 import {serverSideTranslations} from "next-i18next/serverSideTranslations";
 import {Home} from '../component/templates/app/home';
 import nextI18NextConfig from '../next-i18next.config.js';
-import { useEffect } from 'react';
-import $api from '../utils/http';
+
 const HomePage: NextPage<{}> = () => {
-    useEffect(() => {
-        (async () => {
-            const qwe = $api.get("/check");
-            console.info(qwe)
-        })()
-    }, []) 
     return (
         <>
             <Home.Intro />
