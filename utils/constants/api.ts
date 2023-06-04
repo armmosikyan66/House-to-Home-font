5d74f9cb30686429922b0b2afe052537960438d1
Arman <87537406+armmosikyan66@users.noreply.github.com>
2023-06-04 21:20:09 +0400

    Update api.ts

--- utils/constants/api.ts
+++ utils/constants/api.ts
@@ -2,4 +2,4 @@
 //export const API_URL = `http://localhost:5000/api`;
 
 // prd url
-export const API_URL = "https://house-to-home-back.vercel.app/api"
+export const API_URL = "https://housetohome.onrender.com/api"
